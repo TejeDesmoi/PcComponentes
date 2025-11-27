8dff4fc9f40eaa2b884eca8951994774670a511a
Alberto Tejero Mateo <tejeromateo2002@gmail.com>
2025-11-27 19:35:42 +0100

    Add:Motherboard

---
+++ src/Components/Motherboard.java
@@ -0,0 +1,71 @@
+package Components;
+
+public class Motherboard {
+    String name;
+    String brand;
+    String compatibleSocket;
+    String chipset;
+    String ramSlots;
+    String expansionPorts;
+    String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCompatibleSocket() {
+        return compatibleSocket;
+    }
+
+    public void setCompatibleSocket(String compatibleSocket) {
+        this.compatibleSocket = compatibleSocket;
+    }
+
+    public String getChipset() {
+        return chipset;
+    }
+
+    public void setChipset(String chipset) {
+        this.chipset = chipset;
+    }
+
+    public String getRamSlots() {
+        return ramSlots;
+    }
+
+    public void setRamSlots(String ramSlots) {
+        this.ramSlots = ramSlots;
+    }
+
+    public String getExpansionPorts() {
+        return expansionPorts;
+    }
+
+    public void setExpansionPorts(String expansionPorts) {
+        this.expansionPorts = expansionPorts;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+
+
+
+}
