758d0d3def9782ec70372c15b700b9762ad4547c
Alberto Tejero Mateo <tejeromateo2002@gmail.com>
2025-11-27 19:41:22 +0100

    Import domain in main and test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
-
+import Components.*;
 public class Main {
     public static void main(String[] args) {
 
+        CPU cpu1 = new CPU();
+        GPU gpu1 = new GPU();
+        RAM ram1 = new RAM();
+        Motherboard mb1 = new Motherboard();
+        Storage storage = new Storage();
     }
 }
